0a9b3f95cd7f5a9bbebdbb7e9eaa950032a11062
Jonathan Vander Mey <jonathan@vandermey.ca>
2020-12-01 21:39:50 -0500

    Day 1, part 1 solution
    Finding the elements from the list this way takes linear time, although
    it requires sorted data as an input. I'm sure there's a more Rustic way
    to write it but I've got C++ on the brain right now.

--- src/day01.rs
+++ src/day01.rs
@@ -6,6 +6,26 @@ fn parse_input(input: &str) -> Vec<usize> {
 }
 
 #[aoc(day1, part1)]
-fn part1(_vals: &[usize]) -> usize {
+fn part1(vals: &[usize]) -> usize {
+    let mut v = vals.to_vec();
+    v.sort();
+
+    let mut front_iter = v.iter();
+    let mut back_iter = v.iter().rev();
+    let mut front = front_iter.next().unwrap();
+    let mut back = back_iter.next().unwrap();
+
+    while front < back {
+        let sum = front + back;
+
+        if sum == 2020 {
+            return front * back;
+        } else if sum < 2020 {
+            front = front_iter.next().unwrap();
+        } else {
+            back = back_iter.next().unwrap();
+        }
+    }
+
     0
 }
