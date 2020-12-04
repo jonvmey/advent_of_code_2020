6c5fc3c733be80eb157fd845318c273d3230441b
Jonathan Vander Mey <jonathan@vandermey.ca>
2020-12-03 21:21:23 -0500

    Day 3 solution, easier than expected

---
+++ src/day03.rs
@@ -0,0 +1,32 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day3)]
+fn parse_input(input: &str) -> Vec<String> {
+    input.lines().map(|s| s.trim().to_string()).collect()
+}
+
+fn calculate(vals: &[String], right: usize, down: usize) -> usize {
+    vals.into_iter()
+        .step_by(down)
+        .enumerate()
+        .filter(|(index, line)| line.chars().nth((index * right) % line.len()).unwrap() == '#')
+        .count()
+}
+
+#[aoc(day3, part1)]
+fn part1(vals: &[String]) -> usize {
+    calculate(vals, 3, 1)
+}
+
+#[aoc(day3, part2)]
+fn part2(vals: &[String]) -> usize {
+    let counts = [
+        calculate(vals, 1, 1),
+        calculate(vals, 3, 1),
+        calculate(vals, 5, 1),
+        calculate(vals, 7, 1),
+        calculate(vals, 1, 2),
+    ];
+
+    counts.iter().fold(1, |product, count| product * count)
+}
