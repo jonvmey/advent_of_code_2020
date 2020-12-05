a2fd98465cbe1b8ce78e846a6339326218fad05b
Jonathan Vander Mey <jonathan@vandermey.ca>
2020-12-05 13:01:48 -0500

    Add Day 4 Part 2 solution

--- src/day04.rs
+++ src/day04.rs
@@ -3,18 +3,18 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 #[derive(Debug)]
-struct IdInfo {
-    birth_year: Option<()>,
-    issue_year: Option<()>,
-    expiration_year: Option<()>,
-    height: Option<()>,
-    hair_colour: Option<()>,
-    eye_colour: Option<()>,
-    passport_id: Option<()>,
-    country_id: Option<()>,
+struct IdInfo<'a> {
+    birth_year: Option<&'a str>,
+    issue_year: Option<&'a str>,
+    expiration_year: Option<&'a str>,
+    height: Option<&'a str>,
+    hair_colour: Option<&'a str>,
+    eye_colour: Option<&'a str>,
+    passport_id: Option<&'a str>,
+    country_id: Option<&'a str>,
 }
 
-impl IdInfo {
+impl<'a> IdInfo<'a> {
     fn default() -> Self {
         Self {
             birth_year: None,
@@ -28,7 +28,7 @@ impl IdInfo {
         }
     }
 
-    fn new(s: &str) -> Self {
+    fn new(s: &'a str) -> Self {
         lazy_static! {
             static ref RE: Regex = Regex::new(r"([a-z]{3}):(\#?([a-zA-Z0-9])+)").unwrap();
         }
@@ -36,15 +36,17 @@ impl IdInfo {
         let mut id = Self::default();
 
         for captures in RE.captures_iter(s) {
+            let value = Some(captures.get(2).unwrap().as_str());
+
             match &captures[1] {
-                "byr" => id.birth_year = Some(()),
-                "iyr" => id.issue_year = Some(()),
-                "eyr" => id.expiration_year = Some(()),
-                "hgt" => id.height = Some(()),
-                "hcl" => id.hair_colour = Some(()),
-                "ecl" => id.eye_colour = Some(()),
-                "pid" => id.passport_id = Some(()),
-                "cid" => id.country_id = Some(()),
+                "byr" => id.birth_year = value,
+                "iyr" => id.issue_year = value,
+                "eyr" => id.expiration_year = value,
+                "hgt" => id.height = value,
+                "hcl" => id.hair_colour = value,
+                "ecl" => id.eye_colour = value,
+                "pid" => id.passport_id = value,
+                "cid" => id.country_id = value,
                 _ => {}
             }
         }
@@ -61,6 +63,119 @@ impl IdInfo {
             && self.eye_colour.is_some()
             && self.passport_id.is_some()
     }
+
+    fn valid(&self) -> bool {
+        self.valid_birth_year()
+            && self.valid_issue_year()
+            && self.valid_expiration_year()
+            && self.valid_height()
+            && self.valid_hair_colour()
+            && self.valid_eye_colour()
+            && self.valid_passport_id()
+            && self.valid_country_id()
+    }
+
+    fn valid_birth_year(&self) -> bool {
+        // Birth Year - four digits; at least 1920 and at most 2002.
+        if let Some(s) = self.birth_year {
+            if let Ok(year) = s.parse::<i32>() {
+                return (s.len() == 4) && (year >= 1920) && (year <= 2002);
+            }
+        }
+
+        false
+    }
+
+    fn valid_issue_year(&self) -> bool {
+        // Issue Year - four digits; at least 2010 and at most 2020.
+        if let Some(s) = self.issue_year {
+            if let Ok(year) = s.parse::<i32>() {
+                return (s.len() == 4) && (year >= 2010) && (year <= 2020);
+            }
+        }
+
+        false
+    }
+
+    fn valid_expiration_year(&self) -> bool {
+        // Expiration Year - four digits; at least 2020 and at most 2030.
+        if let Some(s) = self.expiration_year {
+            if let Ok(year) = s.parse::<i32>() {
+                return (s.len() == 4) && (year >= 2020) && (year <= 2030);
+            }
+        }
+
+        false
+    }
+
+    fn valid_height(&self) -> bool {
+        // Height) - a number followed by either cm or in:
+        // If cm, the number must be at least 150 and at most 193.
+        // If in, the number must be at least 59 and at most 76.
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"(^\d{2-3})(in|cm)$").unwrap();
+        }
+
+        if let Some(s) = self.height {
+            if let Some(s) = s.strip_suffix("in") {
+                if let Ok(height) = s.parse::<i32>() {
+                    return (height >= 59) && (height <= 76);
+                }
+            }
+
+            if let Some(s) = s.strip_suffix("cm") {
+                if let Ok(height) = s.parse::<i32>() {
+                    return (height >= 150) && (height <= 193);
+                }
+            }
+        }
+
+        false
+    }
+
+    fn valid_hair_colour(&self) -> bool {
+        // Hair Colour - a # followed by exactly six characters 0-9 or a-f.
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^\#[0-9a-f]{6}$").unwrap();
+        }
+
+        if let Some(s) = self.hair_colour {
+            RE.is_match(s)
+        } else {
+            false
+        }
+    }
+
+    fn valid_eye_colour(&self) -> bool {
+        // Eye Colour - exactly one of: amb blu brn gry grn hzl oth.
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^amb|blu|brn|gry|grn|hzl|oth$").unwrap();
+        }
+
+        if let Some(s) = self.eye_colour {
+            RE.is_match(s)
+        } else {
+            false
+        }
+    }
+
+    fn valid_passport_id(&self) -> bool {
+        // Passport ID - a nine-digit number, including leading zeroes.
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^\d{9}$").unwrap();
+        }
+
+        if let Some(s) = self.passport_id {
+            RE.is_match(s)
+        } else {
+            false
+        }
+    }
+
+    fn valid_country_id(&self) -> bool {
+        // Country ID - ignored, missing or not.
+        true
+    }
 }
 
 #[aoc_generator(day4)]
@@ -73,5 +188,13 @@ fn part1(vals: &[String]) -> usize {
     vals.iter()
         .map(|v| IdInfo::new(v))
         .filter(|id| id.required_fields_present())
+        .count()
+}
+
+#[aoc(day4, part2)]
+fn part2(vals: &[String]) -> usize {
+    vals.iter()
+        .map(|v| IdInfo::new(v))
+        .filter(|id| id.valid())
         .count()
 }
